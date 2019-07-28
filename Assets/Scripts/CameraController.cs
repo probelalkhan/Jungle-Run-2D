ef70aa869c68431a1e70d890935d0c23c702b197
Belal Khan <probelalkhan@gmail.com>
2019-07-28 11:23:33 +0900

    Moving Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    private Player player; 
+    private Vector3 lastPosition; 
+    private float distance; 
+
+    void Start()
+    {
+       player = FindObjectOfType<Player>(); 
+       lastPosition = player.transform.position;  
+    }
+
+    void Update()
+    {
+        distance =  player.transform.position.x - lastPosition.x;    
+        transform.position = new Vector3(
+            transform.position.x + distance, 
+            transform.position.y,
+            transform.position.z 
+        );  
+        lastPosition = player.transform.position;  
+    }
+}
