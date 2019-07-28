c70197974c3550b6c44387475f30e3139053f097
Belal Khan <probelalkhan@gmail.com>
2019-07-28 19:21:12 +0900

    Generating Endless Grounds

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+
+    public Transform groundPoint; 
+    public ObjectPooler[] groundPoolers; 
+    private float[] groundWidths; 
+
+    void Start()
+    {
+        groundWidths = new float[groundPoolers.Length];
+        for(int i = 0; i<groundPoolers.Length; i++){
+            groundWidths[i] = groundPoolers[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < groundPoint.position.x){
+            int random = Random.Range(0, groundPoolers.Length);
+            float distance = groundWidths[random]/2; 
+
+            transform.position = new Vector3(
+                transform.position.x + distance, 
+                transform.position.y,
+                transform.position.z
+            );
+
+            GameObject ground = groundPoolers[random].GetPooledGameObject();
+            ground.transform.position = transform.position;
+            ground.SetActive(true);
+
+
+            transform.position = new Vector3(
+                transform.position.x + distance, 
+                transform.position.y,
+                transform.position.z
+            );
+
+        } 
+    }
+}
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    public GameObject pooledObject;
+    public int numberOfObject; 
+    private List<GameObject> gameObjects; 
+
+
+    void Start()
+    {
+        gameObjects = new List<GameObject>(); 
+        
+        for(int i = 0; i<numberOfObject; i++){
+            GameObject gameObject = Instantiate(pooledObject);
+            gameObject.SetActive(false);
+            gameObjects.Add(gameObject);
+        }
+    }
+
+    public GameObject GetPooledGameObject(){
+        foreach(GameObject gameObject in gameObjects){
+            if(!gameObject.activeInHierarchy)
+                return gameObject; 
+        }
+
+        GameObject gameObj = Instantiate(pooledObject);
+        gameObj.SetActive(false);
+        gameObjects.Add(gameObj);
+        return gameObj;
+    }
+
+}
