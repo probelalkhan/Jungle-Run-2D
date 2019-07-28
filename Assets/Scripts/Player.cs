dfa0c5de9584e15209de4fcf8d7fdefb21e0f343
Belal Khan <probelalkhan@gmail.com>
2019-07-28 10:52:01 +0900

    Player Movement and Ground

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed; 
+    private Rigidbody2D rigidBody; 
+
+    void Start()
+    {
+       rigidBody = GetComponent<Rigidbody2D>(); 
+    }
+
+
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+    }
+}
