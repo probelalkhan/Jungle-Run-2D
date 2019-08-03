0eeb38481ee2e1ecbe0d558541ef69e78a597a24
Belal Khan <probelalkhan@gmail.com>
2019-08-03 15:23:24 +0900

    Jump Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,34 @@ public class Player : MonoBehaviour
 {
 
     public float speed; 
+    public float jump; 
+    
+    public LayerMask ground; 
+    
     private Rigidbody2D rigidBody; 
+    private Collider2D playerCollider; 
+    private Animator animator; 
 
     void Start()
     {
        rigidBody = GetComponent<Rigidbody2D>(); 
+       playerCollider = GetComponent<Collider2D>(); 
+       animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+
+        bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
+
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)){
+            if(grounded)
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+        }
+
+        animator.SetBool("Grounded", grounded);
+
     }
 }
