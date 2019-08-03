8b300953435ee03a70e7a4bf992d733eb2c0b67c
Belal Khan <probelalkhan@gmail.com>
2019-08-03 12:03:04 +0900

    Destroying Grounds

---
+++ Assets/Scripts/GroundDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroyer : MonoBehaviour
+{
+
+    private GameObject point; 
+
+    void Start()
+    {
+        point = GameObject.Find("GroundEndPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < point.transform.position.x){
+            gameObject.SetActive(false);
+        }
+    }
+}
