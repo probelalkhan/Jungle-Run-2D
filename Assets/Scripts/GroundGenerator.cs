aa5d9e14f00d2d16a72e657abb156a3499f58a73
Belal Khan <probelalkhan@gmail.com>
2019-08-04 14:11:11 +0900

    Adding Gaps to Grounds

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -6,11 +6,24 @@ public class GroundGenerator : MonoBehaviour
 {
 
     public Transform groundPoint; 
+    public Transform minHeightPoint;
+    public Transform maxHeightPoint; 
+
+    private float minY;
+    private float maxY; 
+
+    public float minGap;
+    public float maxGap;
+
     public ObjectPooler[] groundPoolers; 
     private float[] groundWidths; 
 
     void Start()
     {
+
+        minY = minHeightPoint.position.y; 
+        maxY = maxHeightPoint.position.y; 
+
         groundWidths = new float[groundPoolers.Length];
         for(int i = 0; i<groundPoolers.Length; i++){
             groundWidths[i] = groundPoolers[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
@@ -24,9 +37,12 @@ public class GroundGenerator : MonoBehaviour
             int random = Random.Range(0, groundPoolers.Length);
             float distance = groundWidths[random]/2; 
 
+            float gap = Random.Range(minGap, maxGap);
+            float height = Random.Range(minY, maxY);
+
             transform.position = new Vector3(
-                transform.position.x + distance, 
-                transform.position.y,
+                transform.position.x + distance + gap, 
+                height,
                 transform.position.z
             );
 
