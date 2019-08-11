e89a94650abd73f39e6a7f47ba8372546aef6d7c
Belal Khan <probelalkhan@gmail.com>
2019-08-11 21:00:43 +0900

    Adding Sounds

--- Assets/Scripts/CoinPicker.cs
+++ Assets/Scripts/CoinPicker.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CoinPicker : MonoBehaviour
 {
+    private AudioSource coinPickSound; 
    
     void Start()
     {
-        
+        coinPickSound = GameObject.Find("CoinPickSound").GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.name == "Player"){
             gameObject.SetActive(false);
+            if(coinPickSound.isPlaying){
+                coinPickSound.Stop();
+            }
+            coinPickSound.Play();
             //increase score 
         }
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,9 @@ public class Player : MonoBehaviour
     private Collider2D playerCollider; 
     private Animator animator; 
 
+    public AudioSource deathSound;
+    public AudioSource jumpSound; 
+
     void Start()
     {
        rigidBody = GetComponent<Rigidbody2D>(); 
@@ -29,8 +32,10 @@ public class Player : MonoBehaviour
         bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
 
         if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)){
-            if(grounded)
+            if(grounded){
+                jumpSound.Play();
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+            }
         }
 
         animator.SetBool("Grounded", grounded);
